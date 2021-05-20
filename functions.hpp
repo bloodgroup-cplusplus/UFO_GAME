12b881acfd1d8f0f19f8a5f52484b9fadeaa28a6
cl-rs <81256027+cl-rs@users.noreply.github.com>
2021-05-20 23:06:50 +0530

    Create functions.hpp

---
+++ functions.hpp
@@ -0,0 +1,6 @@
+#include<vector>
+#include<string>
+void display_misses(int misses);
+void greet();
+void end_game(std::string answer, std::string codeword);
+void display_status(std::vector<char>incorrect,std::string answer);
